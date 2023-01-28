7b8c0a927d972792a18e3dc7b0816696d262de16
Murad Məmmədov <muradmemmedov955@gmail.con>
2023-01-29 02:52:11 +0400

    was made basic configuration and added some crud operations

---
+++ resources/views/back/layouts/head.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en" data-layout="vertical" data-topbar="light" data-sidebar="dark" data-sidebar-size="lg" data-sidebar-image="none" data-preloader="disable">
+
+
+<!-- Mirrored from themesbrand.com/velzon/html/material/dashboard-projects.html by HTTrack Website Copier/3.x [XR&CO'2014], Tue, 24 Jan 2023 05:27:18 GMT -->
+<head>
+
+    <meta charset="utf-8" />
+    <title>Projects | Velzon - Admin & Dashboard Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+    <meta content="Themesbrand" name="author" />
+    <!-- App favicon -->
+    <link rel="shortcut icon" href="{{asset('admin-panel')}}/assets/images/favicon.ico">
+
+    <!-- Layout config Js -->
+    <script src="{{asset('admin-panel')}}/assets/js/layout.js"></script>
+    <!-- Bootstrap Css -->
+    <link href="{{asset('admin-panel')}}/assets/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{asset('admin-panel')}}/assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{asset('admin-panel')}}/assets/css/app.min.css" rel="stylesheet" type="text/css" />
+    <!-- custom Css-->
+    <link href="{{asset('admin-panel')}}/assets/css/custom.min.css" rel="stylesheet" type="text/css" />
+
+
+</head>
---
+++ resources/views/back/layouts/script.blade.php
@@ -0,0 +1,18 @@
+ <!-- JAVASCRIPT -->
+ <script src="{{asset('admin-panel')}}/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+ <script src="{{asset('admin-panel')}}/assets/libs/simplebar/simplebar.min.js"></script>
+ <script src="{{asset('admin-panel')}}/assets/libs/node-waves/waves.min.js"></script>
+ <script src="{{asset('admin-panel')}}/assets/libs/feather-icons/feather.min.js"></script>
+ <script src="{{asset('admin-panel')}}/assets/js/pages/plugins/lord-icon-2.1.0.js"></script>
+ <script src="{{asset('admin-panel')}}/assets/js/plugins.js"></script>
+
+ <!-- apexcharts -->
+ <script src="{{asset('admin-panel')}}/assets/libs/apexcharts/apexcharts.min.js"></script>
+
+ <!-- projects js -->
+ <script src="{{asset('admin-panel')}}/assets/js/pages/dashboard-projects.init.js"></script>
+
+ <!-- App js -->
+ <script src="{{asset('admin-panel')}}/assets/js/app.js"></script>
+
+ <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
