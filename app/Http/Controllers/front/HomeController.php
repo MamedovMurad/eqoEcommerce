3c30f76f2adcfe61d84d03d61b5a5cb52e89953c
Eynur21 <eynur@globalsoft.az>
2023-03-15 21:12:58 +0400

    21:12

--- app/Http/Controllers/front/HomeController.php
+++ app/Http/Controllers/front/HomeController.php
@@ -66,6 +66,7 @@ class HomeController extends Controller
         return view('front.products.product');
     }
     public function productCategory($slug){
+        
         return view('front.products.product');
     }
 }
